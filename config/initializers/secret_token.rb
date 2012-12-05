c218dc261d5e21d55aef210e0a51927fe30d64cc
root <root@ubuntu.(none)>
2012-12-05 03:16:44 -0500

    Test Ruby Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestCapistrano::Application.config.secret_token = '64e0034921e6933f4445b4ecee8525e5cd37db6b710e1713b2a940aeb46118442188d930bbe17f0e19e715da420f44678df8ef1fa55691d464359682e988fbc1'
